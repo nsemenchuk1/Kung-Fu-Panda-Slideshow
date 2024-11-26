47d869bac492736da085da497d6e077eb7bfed75
nsemenchuk1 <160068250+nsemenchuk1@users.noreply.github.com>
2024-11-26 10:10:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// Kung Fu Panda Slideshow / Gallery
+
+// Global Variable
+let imgPaths = [
+  "po.png",
+  "tigress.png",
+  "viper.png",
+  "monkey.png",
+  "mantis.png",
+  "crane.png",
+];
+
+// Create image gallery
+let imgContainerEl = document.getElementById("img-container");
+for (let i = 0; i < imgPaths.length; i++) {
+  imgContainerEl.innerHTML += "<img src='images/" + imgPaths[i] + "'>";
+}
